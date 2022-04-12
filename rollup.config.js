12139e5701d092d3d160bafb6cbfb8d5efd96046
alphardex <2582347430@qq.com>
2022-04-12 09:19:06 +0800

    add maku.umd.js

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: "src/maku.ts",
+    output: {
+      name: "maku",
+      file: "./build/maku.umd.js",
+      format: "umd",
+    },
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript(), // so Rollup can convert TypeScript to JavaScript
+      terser(), // so Rollup can minify js code
+    ],
+  },
+];
